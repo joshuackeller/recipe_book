98273b1232c2e6d506ec1390e5b5692eb688c613
joshuackeller <joshuackeller@gmail.com>
2023-08-01 23:05:23 -0600

    try again

--- app/recipes/[recipeId]/page.tsx
+++ app/recipes/[recipeId]/page.tsx
@@ -16,19 +16,19 @@ const Page = async ({ params: { recipeId } }: PageProps) => {
     errorMessage = error;
   }
 
-  return <SingleRecipePage recipe={recipe} />;
+  // return <SingleRecipePage recipe={recipe} />;
 
-  // if (!!recipe?.data) {
-  //   return <SingleRecipePage recipe={recipe?.data} />;
-  // } else {
-  //   return (
-  //     <div>
-  //       <div>Could not find recipe</div>
-  //       {recipe && <div>recipe: {recipe}</div>}
-  //       {errorMessage && <div>error: {errorMessage.toString()}</div>}
-  //     </div>
-  //   );
-  // }
+  if (!!recipe?.data) {
+    return <SingleRecipePage recipe={recipe?.data} />;
+  } else {
+    return (
+      <div>
+        <div>Could not find recipe</div>
+        {recipe && <div>recipe: {recipe}</div>}
+        {errorMessage && <div>error: {errorMessage.toString()}</div>}
+      </div>
+    );
+  }
 };
 
 export default Page;
